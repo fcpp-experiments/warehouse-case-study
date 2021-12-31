fca3e4fbf018ee1160e5a125181f9a76590c7bcc
Giorgio Audrito <giorgio.audrito@gmail.com>
2021-12-31 14:01:39 +0100

    updating fcpp and makefile

--- make.sh
+++ make.sh
@@ -2,4 +2,11 @@
 
 git submodule init
 git submodule update
-fcpp/src/make.sh "$@"
+mkdir -p plot
+cp fcpp/src/extras/plotter/plot.asy plot/
+
+if [ "$1" == "sim" ]; then
+    fcpp/src/make.sh gui run -O -DFCPP_SYSTEM=FCPP_SYSTEM_EMBEDDED graphic
+else
+    fcpp/src/make.sh "$@"
+fi
